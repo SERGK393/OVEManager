a17190b10ff5bbc665116d99cbbde49cdbec5ffd
SERGK393 <sergk393@inbox.ru>
2017-03-17 18:58:35 +0300

    WebView interface

--- app/src/main/java/ru/ovecfo/ovemanager/MainActivity.java
+++ app/src/main/java/ru/ovecfo/ovemanager/MainActivity.java
@@ -2,6 +2,8 @@ package ru.ovecfo.ovemanager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.net.http.SslError;
 import android.os.AsyncTask;
@@ -26,6 +28,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -39,6 +42,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -55,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     WebView webView = null;
     WebView localWebView = null;
     ProgressBar progressBar = null;
+    LinearLayout noNet = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,35 +74,52 @@ public class MainActivity extends AppCompatActivity {
         String uri = "https://www.ove-cfo.ru/mobile";
         String uri_local = "file:///android_asset/index.html";
 
+        noNet=(LinearLayout)findViewById(R.id.noNet);
+        Button noNetClose = (Button)findViewById(R.id.noNet_close);
+        noNetClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         webView.getSettings().setJavaScriptEnabled(true);
+        //webView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
+        webView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        webView.getSettings().setAllowFileAccess( true );
+        webView.getSettings().setAppCacheEnabled( true );
+        webView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT ); // load online by default
+
+        if ( !isNetworkAvailable() ) { // loading offline
+            webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
+        }
+
         localWebView.getSettings().setJavaScriptEnabled(true);
         localWebView.loadUrl(uri_local);
         WebViewClient client = new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url){
-                view.loadUrl(url);
+                if(isNetworkAvailable()) view.loadUrl(url);
+                else showErrorSplash();
                 return true;
-            }
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed(); // Ignore SSL certificate errors
             }
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon){
-                //fadeOut(view);
-                fadeIn(localWebView);
+                //viewFadeOut(view);
+                viewShow(localWebView);
                 progressBar.setVisibility(View.VISIBLE);
             }
             @Override
             public void onPageFinished(WebView view, String url){
-                //fadeIn(view);
-                fadeOut(localWebView);
+                //viewFadeIn(view);
+                viewFadeOut(localWebView);
                 progressBar.setVisibility(View.GONE);
             }
         };
         webView.setWebViewClient(client);
-        webView.loadUrl(uri);
+        if(isNetworkAvailable()) webView.loadUrl(uri);
+        else showErrorSplash();
 
         try {
             ActionBar actionBar = getSupportActionBar();
@@ -109,6 +132,18 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void showErrorSplash(){
+        progressBar.setVisibility(View.GONE);
+        localWebView.setVisibility(View.VISIBLE);
+        viewFadeIn(noNet);
     }
 
     public void setTaskBarColored() {
@@ -134,23 +169,30 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
-    public void fadeIn(View view)
+
+    public void viewShow(View view)
     {
         view.clearAnimation();
         view.setAlpha(1);
-        /*AlphaAnimation animate = new AlphaAnimation(0,1);
-        animate.setDuration(300);
-        //animate.setFillAfter(true);
-        view.startAnimation(animate);*/
         view.setVisibility(View.VISIBLE);
     }
-    public void fadeOut(View view)
+    public void viewFadeIn(View view)
+    {
+        view.clearAnimation();
+        view.setAlpha(1);
+        AlphaAnimation animate = new AlphaAnimation(0,1);
+        animate.setDuration(500);
+        //animate.setFillAfter(true);
+        view.startAnimation(animate);
+        view.setVisibility(View.VISIBLE);
+    }
+    public void viewFadeOut(View view)
     {
         view.clearAnimation();
         view.setAlpha(1);
         view.setVisibility(View.VISIBLE);
         AlphaAnimation animate = new AlphaAnimation(1,0);
-        animate.setDuration(1500);
+        animate.setDuration(500);
         //animate.setFillAfter(true);
         view.startAnimation(animate);
         view.setVisibility(View.GONE);
@@ -251,4 +293,88 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    /*
+    private class SaveHTMLTask extends AsyncTask<String, Void, String> {
+
+        HttpsURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String urlString = "";
+
+        @Override
+        protected String doInBackground(String... params) {
+            // получаем данные с внешнего ресурса
+            String result = "";
+            urlString=params[0];
+            if(isNetworkAvailable()){
+                try {
+
+                    URL url = new URL(urlString);
+
+                    urlConnection = (HttpsURLConnection) url.openConnection();
+                    urlConnection.setReadTimeout(10000);
+                    urlConnection.setConnectTimeout(15000);
+                    urlConnection.setRequestMethod("GET");
+
+                    urlConnection.connect();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+                    StringBuffer buffer = new StringBuffer();
+
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    result = buffer.toString();
+
+                    int oveIndex = urlString.indexOf("ove-cfo.ru/mobile");
+                    if(oveIndex>1) {
+                        oveIndex+=11;
+                        // отрываем поток для записи
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                                openFileOutput(urlString.substring(oveIndex)+"_cache.html", MODE_PRIVATE)));
+                        // пишем данные
+                        bw.write(result);
+                        // закрываем поток
+                        bw.close();
+                    }
+
+                } catch (Exception e) {
+                    Log.e("OVE_ERROR_TAG----------",e.toString());
+                    result = e.toString();
+                    e.printStackTrace();
+                }
+            }else{
+                int oveIndex = urlString.indexOf("ove-cfo.ru/mobile");
+                if(oveIndex>1) {
+                    oveIndex+=11;
+                    try {
+                        // открываем поток для чтения
+                        BufferedReader br = new BufferedReader(new InputStreamReader(
+                                openFileInput(urlString.substring(oveIndex)+"_cache.html")));
+                        String str = "";
+                        // читаем содержимое
+                        while ((str = br.readLine()) != null) {
+                            result += str;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        result = "Network is not avaliable " + e.getMessage();
+                    }
+                }else result = "Network is not avaliable 11 "+urlString;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String strResponse) {
+            super.onPostExecute(strResponse);
+
+            if(!isNetworkAvailable())
+                webView.loadDataWithBaseURL(urlString, strResponse, "text/html", "ru_RU", null);
+        }
+    }*/
 }
